c153c4032bd7573840fcb02b9c9fd7e7d4650c70
38takumi <saitout485@gmail.com>
2021-08-21 11:42:06 +0000

    保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resource :customers, only: [:edit,:update]
+  # resources :customers, only: [:edit,:update]
   get 'customers/my_page' => 'customers#show'
-
+  get '/customers/edit' => 'customers#edit'
+  patch '/customers' => 'customers#update'
 
 end
