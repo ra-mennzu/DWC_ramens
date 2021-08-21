a089bfc65359968ed25e1b4d86cd01ba2773efdf
EC2 Default User <ec2-user@ip-172-31-28-165.ec2.internal>
2021-08-21 17:22:58 +0000

    初回

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+    root to: "homes#top"
+    resources :customers,only: [:index, :show, :edit, :update]
+    resources :orders, only: [:index, :show, :update] do
+      resources :order_items, only: [:update]
+    end
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  
+  scope module: :public do
+    root to: "homes#top"
+    get "/about" => "homes#about", as: "about"
+    resource :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+    resources :orders, only: [:new, :confirm, :create, :complete, :index, :show]
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :create, :update, :destroy, :destroy_all]
+    resources :addresses, only: [:index, :create, :destroy, :edit, :update]
+  end
+
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  devise_for :customers, controllers: {
+    sessions: "public/sessions",
+    passwords: "public/passwords"
+    
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
