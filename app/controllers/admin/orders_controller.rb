19c0c919f92d07bf7260859fe2bffce5b891b544
“naozinn” <naozinn0509@gmail.com>
2021-08-23 07:52:52 +0000

    itemsページ作成、コントローラー記入変更

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,17 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
   end
-  
+
   def update
   end
-  
+
   private
-  
+
   def order_params
+    params.require(:order).permit(:status)
   end
-  
+
 end
