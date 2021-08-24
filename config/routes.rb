db1a51c7d5aecf22dde4acd15bc2419c13408446
38takumi <saitout485@gmail.com>
2021-08-24 06:37:32 +0000

    publicに入れる　deviseのレジストレーションのeditを無効化

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  # devise_for :customers, skip: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :customers, only:[:sessions,:passwords]
 
-  # resources :customers, only: [:edit,:update]
-  get 'customers/my_page' => 'customers#show'
-  get '/customers/edit' => 'customers#edit'
-  patch '/customers' => 'customers#update'
+
+  devise_scope :customers do
+    get '/customers/cancel', to: 'devise/registrations#cancel'
+    get '/customers/sign_up', to: 'devise/registrations#new'
+    patch '/customers', to: 'devise/registrations#update'
+    put '/customers', to: 'devise/registrations#update'
+    delete '/customers', to: 'devise/registrations#destroy'
+    post '/customers', to: 'devise/registrations#create'
+  end
+
+
+  scope module: 'public' do
+    resource :customers, only: [:edit,:update]
+    get 'customers/my_page' => 'customers#show'
+  end
+
+
 
 end
