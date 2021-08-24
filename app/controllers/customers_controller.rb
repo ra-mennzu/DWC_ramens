fc4c1f0140f70c63a6a7f735974c29218acc6c7f
38takumi <saitout485@gmail.com>
2021-08-24 04:30:31 +0000

    仮データ入力

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,6 +6,26 @@ class CustomersController < ApplicationController
     @customer = current_customer
   end
 
+  def edit
+    @customer = current_customer
+  end
+  
+  def update
+    # @customer = Customer.current_customer.find(params[:id])
+    @customer = current_customer
+    @customer.update(customer_params)
+      # flash[:notice] = "You have updated user successfully."
+      # マイページにページに飛ばす
+    redirect_to customers_my_page_path(@customer)
+  end
+  
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name,:last_name_kana,:first_name_kana,:postal_code,
+    :address,:telephone_number,:email)
+  end
+
 
 
 end
