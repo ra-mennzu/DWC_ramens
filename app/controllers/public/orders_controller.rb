cd25477ac85fc4034f5d5735d6559722e5f26cd9
38takumi <saitout485@gmail.com>
2021-08-26 06:24:35 +0000

    注文確認画面途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,10 +9,16 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items = current_customer.cart_items
+    @shipping_cost = Shipping_cost.Find[params:id]
+    @total_payment = current_customer.cart_items.sum(:price)+  @shipping_cost
   end
 
   def create
+    redirect_to orders_path
   end
+
+
 
  private
   # def customer_params
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,10 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
   has_many :items, through: :order_details
+  
+  # お届け先
+  def info
+    name+ postal_code+ address
+  end
+  
 end
