d184d1953d2fdbaa84ab79520d88dc59fd4d63b2
38takumi <saitout485@gmail.com>
2021-08-26 06:46:04 +0000

    customerモデルの記述順序入れ替え

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,14 +1,12 @@
 class Customer < ApplicationRecord
-  has_many :addresses, dependent: :destroy
-  has_many :orders, dependent: :destroy
-  has_many :cart_items, dependent: :destroys
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
 
-
+ has_many :addresses, dependent: :destroy
+ has_many :orders, dependent: :destroy
+ has_many :cart_items, dependent: :destroys
 
 end
