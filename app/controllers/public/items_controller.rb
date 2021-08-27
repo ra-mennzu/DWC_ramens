7c751db25e2973db11512f0b1a673e11800a75fe
38takumi <saitout485@gmail.com>
2021-08-27 09:48:54 +0000

    カートのところコメントアウト

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -7,9 +7,27 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     # @cart_item = CartItem.new
+    
+    # cart_itemのcreateアクション ここから
+    # @cart_item = CartItem.new（crat_item_params）
+    # @cart_item.customer_id = current_user.id
+    # if @cart_item.save
+    #   redirect_to items_path
+    # else
+    #   @items = Item.all.page(params[:page]).reverse_order
+    #   render 'public/items/index'
+    # end
+    # private
+    # def crat_item_params
+      # params.require(:crat_item).permit(:amount)
+    # end
+    # cart_itemのcreateアクション　ここまで
   end
+  
 
-
-
+  # private
+  # def crat_item_params
+  #   params.require(:crat_item).permit(:amount)
+  # end
  
 end
